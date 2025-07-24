73df935babbaf402a304cdc0571527ae4ee81898
Yunseong <dev.yunseong@gmail.com>
2025-07-24 12:26:34 +0900

    feature Product Request Dto - 제약 사항 추가

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -2,15 +2,18 @@ package gift.dto;
 
 import gift.domain.Product;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
 public record ProductRequestDto(
         @Length(max = 15)
+        @NotBlank
         @Pattern(regexp= "^[a-zA-Z0-9가-힣()\\[\\]+&/ _-]+$",
                 message = "The following special characters are allowed: (), [], +, &, /, _, -]")
         String name,
         @Min(0) Integer price,
+        @NotBlank
         @Length(max = 255) String imageUrl) {
 
     public Product toDomain() {
