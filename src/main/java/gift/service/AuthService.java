7eb6b92b126aa1ca33883be61e098feab1d1755b
Yunseong <dev.yunseong@gmail.com>
2025-07-24 14:33:10 +0900

    feature Auth Service - Email Member, Kakao Member를 인증 하는 로직 정리, 구현

--- src/main/java/gift/service/AuthService.java
+++ src/main/java/gift/service/AuthService.java
@@ -1,30 +1,45 @@
 package gift.service;
 
+import gift.domain.member.KakaoMember;
 import gift.dto.AuthResponseDto;
 import gift.dto.MemberRequestDto;
 import gift.domain.member.Member;
 import gift.security.JwtTokenProvider;
+import gift.service.member.EmailMemberService;
+import gift.service.member.KakaoMemberService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
-    private final MemberService memberService;
+    private final EmailMemberService emailMemberService;
+    private final KakaoMemberService kakaoMemberService;
+    private final KakaoLoginService kakaoLoginService;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthService(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
-        this.memberService = memberService;
+    public AuthService(EmailMemberService emailMemberService, KakaoMemberService kakaoMemberService, KakaoLoginService kakaoLoginService, JwtTokenProvider jwtTokenProvider) {
+        this.emailMemberService = emailMemberService;
+        this.kakaoMemberService = kakaoMemberService;
+        this.kakaoLoginService = kakaoLoginService;
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
     public AuthResponseDto register(MemberRequestDto memberRequestDto) {
-        Member member = memberService.creatMember(memberRequestDto);
+        Member member = emailMemberService.creatMember(memberRequestDto);
         String token = jwtTokenProvider.makeJwtToken(member.getId());
         return new AuthResponseDto(token);
     }
 
     public AuthResponseDto login(MemberRequestDto memberRequestDto) {
-        Member member = memberService.login(memberRequestDto);
+        Member member = emailMemberService.login(memberRequestDto);
+        String token = jwtTokenProvider.makeJwtToken(member.getId());
+        return new AuthResponseDto(token);
+    }
+
+    public AuthResponseDto registerOrLoginKakao(String authorizeCode) {
+        String accessToken = kakaoLoginService.getAccessToken(authorizeCode);
+        KakaoMember kakaoMember = kakaoLoginService.getKakaoMember(accessToken);
+        Member member = kakaoMemberService.registerOrLogin(kakaoMember);
         String token = jwtTokenProvider.makeJwtToken(member.getId());
         return new AuthResponseDto(token);
     }
