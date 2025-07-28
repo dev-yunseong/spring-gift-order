54006783f3dcf45b20e45c0b0e6b5b0b511c4306
Yunseong <dev.yunseong@gmail.com>
2025-07-28 14:57:46 +0900

    feature add OptionBuyingRepository for option buying data access

---
+++ src/main/java/gift/repository/OptionBuyingRepository.java
@@ -0,0 +1,7 @@
+package gift.repository;
+
+import gift.entity.OptionBuyingEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionBuyingRepository extends JpaRepository<OptionBuyingEntity, Long> {
+}
